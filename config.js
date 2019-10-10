5d8c0a3b7104ae1d452ac13950fe86795666dbee
merabchik83@gmail.com <merabchik>
2019-10-10 16:44:23 +0400

    Add database key to config

--- config.js
+++ config.js
@@ -2,12 +2,24 @@ const config = (pEnv) => {
         if(pEnv == 'development') {
             return {
                 host: '127.0.01',
-                port: 3000
+                port: 3000,
+                database: {
+                    host: 'localhost',
+                    username: 'root',
+                    password: 'vertrigo',
+                    database: 'hsms'
+                }
             };
         }else if(pEnv == 'production') {
             return {
                 host: '127.0.01',
-                port: 80
+                port: 80,
+                database: {
+                    host: 'localhost',
+                    username: 'root',
+                    password: 'vertrigo',
+                    database: 'hsms'
+                }
             };
         }
     }
